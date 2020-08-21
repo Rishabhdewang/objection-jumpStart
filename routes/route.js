39d27f8f8b6fddec828997b30bba7bc86ce61ecd
Rishabh <48730812+Rishabhdewang@users.noreply.github.com>
2020-08-21 23:42:16 +0530

    Update route.js

--- routes/route.js
+++ routes/route.js
@@ -7,11 +7,26 @@ app.get('/allusers',(req,res) => {
     console.log("Users are");
 })
 
-app.post('/adduser', async (req,res ) => {
-    console.log(req.body);
-    let data  =  await Post.query().insert(req.body);
-    
+app.get('/user/:id', async(req,res) => {
+    const user = await Posts.query().findById(req.params.id);
 })
 
+app.post('/adduser', async (req,res ) => {
+    let data  =  await Posts.query().insert(req.body);
+    console.log(data);
+    res.send("data added",data);   
+});
+
+app.post('/update/:id', async (req,res) => {
+    console.body(req.body,req.params.id);
+    let updated = await Posts.query().findById(req.params.id)
+    .patch(req.body);
+    res.send(updated);
+});
+
+app.delete('/delete/:id',async(req,res) => {
+    const deleted = await Posts.query().deleteById(req.params.id);
+    res.send(deleted)
+});
 
 module.exports = app;
